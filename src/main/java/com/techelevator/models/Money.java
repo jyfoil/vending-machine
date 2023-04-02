56b4f84492ade8a03998a7b8338685495272cdc7
Justin Yang <yjust16@gmail.com>
2023-04-01 20:34:19 -0700

    Fix merge issues for auditing purchases

--- src/main/java/com/techelevator/models/Money.java
+++ src/main/java/com/techelevator/models/Money.java
@@ -69,41 +69,6 @@ public class Money {
         }
     }
 
-    public int[] calculateChange(BigDecimal amount) {
-        int dollars = amount.intValue();
-        BigDecimal remainingCentsBD = amount.subtract(new BigDecimal(dollars)).multiply(new BigDecimal("100"));
-        int remainingCents = remainingCentsBD.intValue();
-
-        int quarters = remainingCents / 25;
-        remainingCents %= 25;
-
-        int dimes = remainingCents / 10;
-        remainingCents %= 10;
-
-        int nickels = remainingCents / 5;
-        remainingCents %= 5;
-
-        return new int[] {dollars, quarters, dimes, nickels};
-    }
-
-    public void displayChange(BigDecimal amount) {
-        int[] coins = calculateChange(amount);
-        System.out.println();
-        System.out.print("Your change is: ");
-        if (coins[0] > 0) {
-            System.out.print(coins[0] + " dollar(s) ");
-        }
-        if (coins[1] > 0) {
-            System.out.print(coins[1] + " quarter(s) ");
-        }
-        if (coins[2] > 0) {
-            System.out.print(coins[2] + " dime(s) ");
-        }
-        if (coins[3] > 0) {
-            System.out.print(coins[3] + " nickel(s) ");
-        }
-        System.out.println();
-    }
 }
 
 
